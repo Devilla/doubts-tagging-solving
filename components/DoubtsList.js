9ef96165fcb05f8110cd35f71912406eb94aef5e
devilla <dev.koold@gmail.com>
2018-12-03 15:33:16 +0530

    scrollable view

--- components/DoubtsList.js
+++ components/DoubtsList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableHighlight, Button} from 'react-native';
+import {View, ScrollView, Text, TouchableHighlight, Button} from 'react-native';
 
 export default class DoubtsList extends Component {
 
@@ -18,18 +18,31 @@ handleAnswerPress = () => {
     const {questions} = this.state;
     return (
         <View>
+        <ScrollView>
           <Text style={{fontSize: 50,textAlign: 'center'}}>Answer Doubts</Text>
+          <Text style={{fontSize: 15, margin:15, textAlign: 'center'}}>High Priority</Text>
             <View>
             {questions.reverse().map((prop, key) => {
                 return (
                   <View key={key}>
-                  <Text  style={{margin:50}} key={key}>question: {key+1} {prop}</Text>
+                  <Text  style={{margin:50}} key={key}>Question: {key+1} {prop}</Text>
                   <Button onPress={this.handleAnswerPress} title="Answer" ></Button>
                   </View>
                 );
              })}
-
             </View>
+            <Text style={{fontSize: 15, margin:15, textAlign: 'center'}}>All Questions</Text>
+              <View>
+              {questions.reverse().map((prop, key) => {
+                  return (
+                    <View key={key}>
+                    <Text  style={{margin:50}} key={key}>Question: {key+1} {prop}</Text>
+                    <Button onPress={this.handleAnswerPress} title="Answer" ></Button>
+                    </View>
+                  );
+               })}
+              </View>
+          </ScrollView>
         </View>
     );
   }
